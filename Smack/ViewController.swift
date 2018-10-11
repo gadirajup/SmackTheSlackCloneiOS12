b878984c1b924fb1a273a484dde60aa18f90bde8
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2018-10-11 01:02:56 -0700

    init commit

---
+++ Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Prudhvi Gadiraju on 10/11/18.
+//  Copyright © 2018 Kore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
